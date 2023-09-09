63bd3af7f4816a43bfda05c71f6c1d0d9cc29922
c-keasling <132222567+c-keasling@users.noreply.github.com>
2023-09-09 15:41:07 -0700

    added annimation effects to buttons

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHtml = this.innerHTML;
     makeSound(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
   });
 }
 
 document.addEventListener("keydown", function(event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -46,6 +48,13 @@ function makeSound(key) {
   }
 }
 
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+} // adds animation
 
 
 
